0b94330cf3b89f4501a499e67fed317eae328e36
muhammed umar <muhammedumaralx@gmail.com>
2023-10-09 22:18:54 +0100

    Task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-import asyncio
-import random
-
 """
 This module define an Asynchronous function `wait_random`
 """
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
