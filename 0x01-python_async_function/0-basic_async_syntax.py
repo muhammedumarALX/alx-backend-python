1839f7bd3a65e10a4e7dfeea738832abba36384e
muhammed umar <muhammedumaralx@gmail.com>
2023-10-09 22:15:29 +0100

    Task 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,12 +3,13 @@ import asyncio
 import random
 
 """
-Asynchronous function `wait_random`
+This module define an Asynchronous function `wait_random`
 """
 
 
 async def wait_random(max_delay: int = 10) -> float:
     """
+    Asynchronous coroutine that waits for a random delay
     Returns `float`
     """
     delay = random.uniform(0, max_delay)
