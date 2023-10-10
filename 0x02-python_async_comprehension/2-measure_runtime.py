b80a1735681043f233ce733a79c18ea654a08351
muhammed umar <muhammedumaralx@gmail.com>
2023-10-10 22:52:06 +0100

    Task 3

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module defines an asynchronous function
+"""
+import asyncio
+import time
+
+
+async def measure_runtime():
+    """
+    function that measures runtime and returns a float
+    """
+    async_compre = __import__("1-async_comprehension").async_comprehension
+    tasks = [async_compre() for _ in range(4)]
+
+    start_time = time.time()
+
+    await asyncio.gather(*tasks)
+
+    end_time = time.time()
+
+    total_runtime = end_time - start_time
+
+    return total_runtime
